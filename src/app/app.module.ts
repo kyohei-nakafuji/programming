acb70dbeb9fc34a9f13f386727694dc59ff07386
kyohei <kyohei.nakafuji@gmail.com>
2017-03-23 23:34:30 +0900

    コンポーネント追加　ヘッダー

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TopMainComponent } from './top-main/top-main.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopMainComponent
+    TopMainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
