4e88bb9a2f63f419b1e9f490f10c9ec21cfe9ff4
kyohei <kyohei.nakafuji@gmail.com>
2017-03-23 23:35:35 +0900

    コンポーネント追加　フッター

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { TopMainComponent } from './top-main/top-main.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopMainComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
