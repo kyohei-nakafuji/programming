34533a9454dc96120fe79904df3a6286e5a4b444
kyohei <kyohei.nakafuji@gmail.com>
2017-04-01 00:24:20 +0900

    コース詳細説明文引き継ぎ追加

--- src/app/top/course-introduction-detail/course-introduction-detail.component.ts
+++ src/app/top/course-introduction-detail/course-introduction-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-course-introduction-detail',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-introduction-detail.component.scss']
 })
 export class CourseIntroductionDetailComponent implements OnInit {
+  @Input() title: string;
+  @Input() imagePath: string;
+  @Input() content: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
   }
